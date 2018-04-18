8de958eb2cccbc23f4a501395b3e27dc46ac635c
Soorya Narayan <nayaran.ayroos@gmail.com>
2018-04-18 11:58:26 +0530

    Change tester to mcts

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 int main() {
 	GipfState state = GipfState();
 
-	Minimax<GipfState, GipfMove> a(0.1, 30);
-	Minimax<GipfState, GipfMove> b(0.1);
+	MonteCarloTreeSearch<GipfState, GipfMove> a(0.1, 30);
+	MonteCarloTreeSearch<GipfState, GipfMove> b(0.1);
 
 	// state, player a, player b, no of games, verbose, generate gif
 	Tester<GipfState, GipfMove> tester(&state, a, b, 1, false, true);
